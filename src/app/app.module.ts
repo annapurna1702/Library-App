0767e298c484bf05199ecea887aef4fe854e4ba3
annapurna1702 <annapurnapadmaprema@gmail.com>
2022-07-06 19:45:35 +0530

    Library Management done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddbooksComponent } from './addbooks/addbooks.component';
+import { SearchbooksComponent } from './searchbooks/searchbooks.component';
+import { ViewbooksComponent } from './viewbooks/viewbooks.component';
+import { DeletebooksComponent } from './deletebooks/deletebooks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddbooksComponent,
+    SearchbooksComponent,
+    ViewbooksComponent,
+    DeletebooksComponent
   ],
   imports: [
     BrowserModule,
